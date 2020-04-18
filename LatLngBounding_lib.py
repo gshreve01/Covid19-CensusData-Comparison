e304527cd344ed24200f223ff5ab64b7b0aed328
Gregory Shreve <gshreve1966@gmail.com>
2020-04-18 18:14:25 -0400

    Working on determining boundaries around Charlotte

---
+++ LatLngBounding_lib.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 15:54:41 2020
+
+@author: gshre
+"""
+
+import requests
+import numpy as np
+from math import radians, sin, cos, acos, atan2, sqrt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+from pprint import pprint
+
+# Based on google search
+#https://stackoverflow.com/questions/837872/calculate-distance-in-meters-when-you-know-longitude-and-latitude-in-java
+def Lat_Lng_Distance_From(lat1, lng1, lat2, lng2):
+    earthRadius = 6371000 # meters
+    dLat = radians(lat2-lat1);
+    dLng = radians(lng2-lng1);
+    a = sin(dLat/2) * sin(dLat/2) + cos(radians(lat1)) * cos(radians(lat2)) * sin(dLng/2) * sin(dLng/2)
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    dist = (float) (earthRadius * c)
+
+    return dist;
+    
+
+def Load_Charlotte_Boundaries():
+    url = "https://nominatim.openstreetmap.org/search.php?q=Charlotte%20NC&polygon_geojson=1&format=json"
+    response = requests.get(url).json()
+    
+    #pprint(response)
+    return response
+
+def bounding_box(points):
+    xmin = min(points[1])
+    xmax = max(points[1])
+    ymin = min(points[0])
+    ymax = max(points[0])
+    
+    return {
+       'topleft' : { 'x': xmin, 'y' : ymax },
+       'topright' : { 'x' : xmax, 'y' : ymax },
+       'bottomleft' : { 'x' : xmin, 'y' : ymin },
+       'bottomright' : { 'x' : xmax, 'y' : ymin }}
+
+    
+def Is_Point_Within_Charlotte_Boundary(lat, lng):
+    # define variable as global to prevent multiple loads of geo data
+    global charlote_boundary_polygon
+    if not 'charlote_boundary_polygon' in globals():
+        print("Loading Geo Data boundaries for Charlotte")
+        geo_data = Load_Charlotte_Boundaries()
+        
+        lng=[]
+        lat=[]
+        coordintates = geo_data[0]["geojson"]["coordinates"][0]
+        for x in range(len(coordintates)):
+            lng.append(coordintates[x][0])
+            lat.append(coordintates[x][1])
+        
+        
+        lons_lats_vect = np.column_stack((lat, lng)) # Reshape coordinates
+        charlote_boundary_polygon = Polygon(lons_lats_vect) # create polyg        
+    return charlote_boundary_polygon.contains(point)    
+
+
+point = Point(35.227085,-80.843124) # Charlotte Center
+print(Is_Point_Within_Charlotte_Boundary(35.227085, -80.043124)) # check if polygon contains point
+print(Is_Point_Within_Charlotte_Boundary(35.227085, -80.043124)) # check if polygon contains point
+
+print(type(charlote_boundary_polygon))
+
+dist = Lat_Lng_Distance_From(36.0001, -80.0001, 36.1001, -80.0001)
+print(dist)
+
+charlotte_box = bounding_box(charlote_boundary_polygon.exterior.coords.xy)
+print(charlotte_box)
+
+ 
+ 
+	
